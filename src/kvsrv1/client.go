c05ac4641b01231419d5549ea913a8d7335ebf33
xypp3 <petrkmilev@gmail.com>
2025-07-03 00:58:47 +0200

    feat: Client get command

--- src/kvsrv1/client.go
+++ src/kvsrv1/client.go
@@ -1,11 +1,12 @@
 package kvsrv
 
 import (
+	"time"
+
 	"6.5840/kvsrv1/rpc"
 	"6.5840/kvtest1"
 	"6.5840/tester1"
 )
-
 
 type Clerk struct {
 	clnt   *tester.Clnt
@@ -30,7 +31,20 @@ func MakeClerk(clnt *tester.Clnt, server string) kvtest.IKVClerk {
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 	// You will have to modify this function.
-	return "", 0, rpc.ErrNoKey
+	args := rpc.GetArgs{Key: key}
+	reply := rpc.GetReply{}
+	for {
+		ok := ck.clnt.Call(ck.server, "KVServer.Get", &args, &reply)
+		if !ok {
+			time.Sleep(time.Millisecond * 100)
+		}
+		if reply.Err == rpc.OK {
+			return reply.Value, reply.Version, reply.Err
+		}
+		if reply.Err == rpc.ErrNoKey {
+			return "", 0, reply.Err
+		}
+	}
 }
 
 // Put updates key with value only if the version in the
