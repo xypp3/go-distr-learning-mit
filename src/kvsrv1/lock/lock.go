ff5f041b84614cbe7e2dd95591b0bda51c847d9f
xypp3 <petrkmilev@gmail.com>
2025-07-03 16:12:22 +0200

    exper: With locking and IDs???

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,6 +1,9 @@
 package lock
 
 import (
+	"sync"
+	"time"
+
 	"6.5840/kvtest1"
 )
 
@@ -11,6 +14,10 @@ type Lock struct {
 	// MakeLock().
 	ck kvtest.IKVClerk
 	// You may add code here
+	mu   sync.Mutex
+	lock map[string]bool
+	id   string
+	l    string
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -21,13 +28,32 @@ type Lock struct {
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
 	lk := &Lock{ck: ck}
 	// You may add code here
+	lk.lock = make(map[string]bool)
+	lk.lock[l] = false
+	lk.id = kvtest.RandValue(8)
+	lk.l = l
+
 	return lk
 }
 
 func (lk *Lock) Acquire() {
 	// Your code here
+	for {
+		lk.mu.Lock()
+		v, _ := lk.lock[lk.l]
+		if !v {
+			v = true
+			break
+		}
+		lk.mu.Unlock()
+		time.Sleep(10 * time.Millisecond)
+	}
+	lk.mu.Unlock()
 }
 
 func (lk *Lock) Release() {
 	// Your code here
+	lk.mu.Lock()
+	defer lk.mu.Unlock()
+	lk.lock[lk.l] = false
 }
